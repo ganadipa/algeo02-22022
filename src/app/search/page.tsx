85901f6f3a6fa800735e025da6a44c3c3b079607
AldyDPP <possiblepiano@gmail.com>
2023-11-19 21:07:02 +0700

    fix: Grammar issue
    tak bisa bhs enggres

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-wrap gap-8">
+    <div className="flex justify-center flex-wrap gap-8">
       <div className="max-w-[700px] rounded bg-white p-3 font-semibold ">
-        For the main search (which is at /home), upload an image for query and
-        upload a folder (dataset) which ALL inside the folder is a type of image
-        is a MUST. For searching the similiarity between the query and each
-        image in the dataset uploaded you can use search method by color or by
-        texture.
+        For the main search (which is at /home), upload an image as a query 
+        and then upload a folder (dataset) which contains only images of 
+        extensions .png, .jpg, or .jpeg. Wait a few moments before uploading 
+        is done, and then you can simply click the "color" or the "texture" 
+        button to search an image using the respective method.
+
       </div>
       <div className="max-w-[700px] rounded bg-white p-3 font-semibold">
-        For the real time search, (which is at /realtime), you only need to
-        upload a folder (dataset) which ALL inside the folder is a type of
-        image.
+        For the real time search, (which is at the /realtime page), you only 
+        need to upload a folder (dataset) which contains images as well.
         <br />
-        As well as the main search you can also choose to search similiarities
-        using its color or using its texture.
+        Like the main search you can also choose to search similiarities
+        by color or by texture.
         <br />
         <br />
         By default, the search query will be automatically fetched if you dont
